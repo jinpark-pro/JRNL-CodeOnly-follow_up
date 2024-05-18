40d0dda41664660b86ed21fcde21807c90b494a5
Jin Park <jinpark1504@gmail.com>
2024-05-18 18:35:45 +0900

    Add BarButtonItem to New Entry

--- JRNL-CodeOnly-follow_up/AddJournalViewController.swift
+++ JRNL-CodeOnly-follow_up/AddJournalViewController.swift
@@ -10,5 +10,19 @@ import UIKit
 class AddJournalViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.title = "New Entry"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(save))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+    }
+    
+    @objc func save() {
+        
+    }
+    
+    @objc func cancel() {
+        dismiss(animated: true)
     }
 }
