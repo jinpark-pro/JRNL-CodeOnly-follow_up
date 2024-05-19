8e31aba1c18e892fdaf3f9a73053870ec90ccacf
Jin Park <jinpark1504@gmail.com>
2024-05-19 17:22:05 +0900

    Add cells to detail view and set the height

--- JRNL-CodeOnly-follow_up/JournalDetailTableViewController.swift
+++ JRNL-CodeOnly-follow_up/JournalDetailTableViewController.swift
@@ -22,18 +22,31 @@ class JournalDetailTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(UITableView.self, forCellReuseIdentifier: "cell")
         navigationItem.title = "Detail"
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 7
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 1: return 60
+        case 3: return 150
+        case 4, 5: return 316
+        default: return 44.5
+        }
     }
 }
