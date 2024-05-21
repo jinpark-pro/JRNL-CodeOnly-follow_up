d5dab03168441956fcd8abd08c0d9107efd28059
Jin Park <jinpark1504@gmail.com>
2024-05-21 09:08:43 +0900

    Create SharedData

---
+++ JRNL-CodeOnly-follow_up/Model/SharedData.swift
@@ -0,0 +1,32 @@
+//
+//  SharedData.swift
+//  JRNL-CodeOnly-follow_up
+//
+//  Created by Jungjin Park on 2024-05-21.
+//
+
+import UIKit
+
+class SharedData {
+    static let shared = SharedData()
+    private var journalEntries: [JournalEntry]
+    
+    private init() {
+        journalEntries = []
+    }
+    
+    func numberOfJournalEntries() -> Int {
+        journalEntries.count
+    }
+    
+    func getJournalEntry(index: Int) -> JournalEntry {
+        journalEntries[index]
+    }
+    
+    func getAllJournalEntries() -> [JournalEntry] {
+        let readOnlyJournalEntries = journalEntries
+        return readOnlyJournalEntries
+    }
+    
+    
+}
