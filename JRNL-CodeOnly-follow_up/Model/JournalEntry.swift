1b40d51421c5b6f4ee3dbabb2991cf750bc9dee2
Jin Park <jinpark1504@gmail.com>
2024-06-02 07:41:30 +0900

    Clean up the sample data

--- JRNL-CodeOnly-follow_up/Model/JournalEntry.swift
+++ JRNL-CodeOnly-follow_up/Model/JournalEntry.swift
@@ -47,26 +47,3 @@ class JournalEntry: NSObject, MKAnnotation {
         entryBody
     }
 }
-
-// MARK: - Sample Data
-//struct SampleJournalEntryData {
-//    var journalEntries: [JournalEntry] = []
-//    
-//    mutating func createSampleJournalEntryData() {
-//        let photo1 = UIImage(systemName: "sun.max")
-//        let photo2 = UIImage(systemName: "cloud")
-//        let photo3 = UIImage(systemName: "cloud.sun")
-//        
-//        guard let journalEntry1 = JournalEntry(rating: 5, title: "Good", body: "Today is good day.", photo: photo1) else {
-//            fatalError("Unable to instantiate journalEntry1")
-//        }
-//        guard let journalEntry2 = JournalEntry(rating: 0, title: "Bad", body: "Today is bad day.", photo: photo2, latitude: 37.3318, longitude: -122.0312) else {
-//            fatalError("Unable to instantiate journalEntry2")
-//        }
-//        guard let journalEntry3 = JournalEntry(rating: 3, title: "Ok", body: "Today is ok day.", photo: photo3) else {
-//            fatalError("Unable to instantiate journalEntry3")
-//        }
-//        
-//        return journalEntries += [journalEntry1, journalEntry2, journalEntry3]
-//    }
-//}
