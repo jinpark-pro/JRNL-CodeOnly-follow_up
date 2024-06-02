2d2c2e2e0fccad7040b821e1ccd7700575a58b1f
Jin Park <jinpark1504@gmail.com>
2024-06-02 11:05:18 +0900

    Add Codable to JournalEntry
    For saving data to JSON, add `Codable` protocol to the JournalEntry model for decoding and encoding, and modify the photo for using photoData.

--- JRNL-CodeOnly-follow_up/Model/JournalEntry.swift
+++ JRNL-CodeOnly-follow_up/Model/JournalEntry.swift
@@ -8,7 +8,7 @@
 import UIKit
 import MapKit
 
-class JournalEntry: NSObject, MKAnnotation {
+class JournalEntry: NSObject, MKAnnotation, Codable {
     var coordinate: CLLocationCoordinate2D {
         guard let lat = latitude,
               let long = longitude else {
@@ -22,7 +22,16 @@ class JournalEntry: NSObject, MKAnnotation {
     let rating: Int
     let entryTitle: String
     let entryBody: String
-    let photo: UIImage?
+    var photo: UIImage? {
+        get {
+            guard let data = photoData else { return nil }
+            return UIImage(data: data)
+        }
+        set {
+            photoData = newValue?.jpegData(compressionQuality: 1.0)
+        }
+    }
+    private var photoData: Data?
     let latitude: Double?
     let longitude: Double?
     
@@ -35,7 +44,8 @@ class JournalEntry: NSObject, MKAnnotation {
         self.rating = rating
         self.entryTitle = title
         self.entryBody = body
-        self.photo = photo
+//        self.photo = photo
+        self.photoData = photo?.jpegData(compressionQuality: 1.0)
         self.latitude = latitude
         self.longitude = longitude
     }
@@ -46,4 +56,31 @@ class JournalEntry: NSObject, MKAnnotation {
     var subtitle: String? {
         entryBody
     }
+    
+    enum CodingKeys: String, CodingKey {
+        case date, rating, entryTitle, entryBody, photoData, latitude, longitude
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        date = try container.decode(Date.self, forKey: .date)
+        rating = try container.decode(Int.self, forKey: .rating)
+        entryTitle = try container.decode(String.self, forKey: .entryTitle)
+        entryBody = try container.decode(String.self, forKey: .entryBody)
+        photoData = try container.decode(Data.self, forKey: .photoData)
+        latitude = try container.decode(Double.self, forKey: .latitude)
+        longitude = try container.decode(Double.self, forKey: .longitude)
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(date, forKey: .date)
+        try container.encode(rating, forKey: .rating)
+        try container.encode(entryTitle, forKey: .entryTitle)
+        try container.encode(entryBody, forKey: .entryBody)
+        try container.encode(photoData, forKey: .photoData)
+        try container.encode(latitude, forKey: .latitude)
+        try container.encode(longitude, forKey: .longitude)
+    }
+        
 }
