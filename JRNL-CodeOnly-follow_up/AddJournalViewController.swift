ce8be5a83a25b1fe2b48182ea47cb04fa9a3e4c8
Jin Park <jinpark1504@gmail.com>
2024-06-10 01:00:10 +0900

    Save selected rating

--- JRNL-CodeOnly-follow_up/AddJournalViewController.swift
+++ JRNL-CodeOnly-follow_up/AddJournalViewController.swift
@@ -37,7 +37,7 @@ class AddJournalViewController: UIViewController, CLLocationManagerDelegate, UIT
         return stackView
     }()
     
-    private lazy var ratingView: UIStackView = {
+    private lazy var ratingView: RatingView = {
         let ratingView = RatingView(frame: CGRect(x: 0, y: 0, width: 252, height: 44))
         ratingView.distribution = .fillEqually
         return ratingView
@@ -185,10 +185,11 @@ class AddJournalViewController: UIViewController, CLLocationManagerDelegate, UIT
               let body = bodyTextView.text, !body.isEmpty else {
             return
         }
+        let rating = ratingView.rating
         let lat = currentLocation?.coordinate.latitude
         let long = currentLocation?.coordinate.longitude
         
-        let journalEntry = JournalEntry(rating: 3, title: title, body: body, photo: UIImage(systemName: "face.smiling")?.withRenderingMode(.alwaysOriginal), latitude: lat, longitude: long)!
+        let journalEntry = JournalEntry(rating: rating, title: title, body: body, photo: UIImage(systemName: "face.smiling")?.withRenderingMode(.alwaysOriginal), latitude: lat, longitude: long)!
         
         delegate?.saveJournalEntry(journalEntry)
         dismiss(animated: true)
